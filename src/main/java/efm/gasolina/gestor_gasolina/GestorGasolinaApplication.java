c26248e76bc46d5a89f8ff32f930a29bb41ce352
Mariopinsel <pmlaser25@gmail.com>
2026-02-23 17:10:32 -0500

    Primer commit

---
+++ src/main/java/efm/gasolina/gestor_gasolina/GestorGasolinaApplication.java
@@ -0,0 +1,13 @@
+package efm.gasolina.gestor_gasolina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorGasolinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorGasolinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/efm/gasolina/gestor_gasolina/GestorGasolinaApplicationTests.java
@@ -0,0 +1,13 @@
+package efm.gasolina.gestor_gasolina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorGasolinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
