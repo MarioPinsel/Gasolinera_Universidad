3eb320ff473d3e1fd1f3207a0adaebf3dad57381
EST-06 <Esteguz06@gmail.com>
2026-04-05 16:55:36 -0500

    Aprobao

--- GestorSpring/src/main/java/efm/gasolina/gestor_gasolina/service/opStation/OpDeliveryService.java
+++ GestorSpring/src/main/java/efm/gasolina/gestor_gasolina/service/opStation/OpDeliveryService.java
@@ -1,22 +1,57 @@
 package efm.gasolina.gestor_gasolina.service.opStation;
 
+import efm.gasolina.gestor_gasolina.handler.ExceedsCapacityException;
+import efm.gasolina.gestor_gasolina.model.station.Station;
 import efm.gasolina.gestor_gasolina.model.wholesaler.Delivery;
+import efm.gasolina.gestor_gasolina.repository.station.StationRepository;
 import efm.gasolina.gestor_gasolina.repository.wholesaler.DeliveryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class OpDeliveryService {
 
-    @Autowired
-    private DeliveryRepository deliveryRepository;
+    private final DeliveryRepository deliveryRepository;
+    private final StationRepository stationRepository;
 
-    public void acceptDelivery(Long id) {
+    public OpDeliveryService(DeliveryRepository deliveryRepository, StationRepository stationRepository) {
+        this.deliveryRepository = deliveryRepository;
+        this.stationRepository = stationRepository;
+    }
+
+     public void acceptDelivery(Long id) {
         Delivery delivery = deliveryRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Entrega no encontrada"));
+
+        Station station = delivery.getStation();
+        Integer volume = delivery.getVolume();
+        String fuelType = delivery.getFuelType();
+
+        if ("Corriente".equals(fuelType)) {
+            if (volume > station.getRegular_capacity() ||
+                    station.getRegular_quantity() + volume > station.getRegular_capacity()) {
+                throw new ExceedsCapacityException();
+            }
+            station.setRegular_quantity(station.getRegular_quantity() + volume);
+        }
+
+        else if ("Diesel".equals(fuelType)) {
+            if (volume > station.getDiesel_capacity() ||
+                    station.getDiesel_quantity() + volume > station.getDiesel_capacity()) {
+                throw new ExceedsCapacityException();
+            }
+            station.setDiesel_quantity(station.getDiesel_quantity() + volume);
+        }
+        else {
+            throw new IllegalArgumentException("Tipo de combustible no válido: " + fuelType);
+        }
+    
         delivery.setStatus("ACCEPTED");
+        stationRepository.save(station);
         deliveryRepository.save(delivery);
     }
 
