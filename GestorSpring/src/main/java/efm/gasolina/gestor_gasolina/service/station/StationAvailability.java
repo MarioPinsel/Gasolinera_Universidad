b2d08e145f12839b89b274bf44e79675345d42bc
EST-06 <Esteguz06@gmail.com>
2026-05-13 20:57:07 -0500

    Estadisticas

--- GestorSpring/src/main/java/efm/gasolina/gestor_gasolina/service/station/StationAvailability.java
+++ GestorSpring/src/main/java/efm/gasolina/gestor_gasolina/service/station/StationAvailability.java
@@ -1,9 +1,10 @@
 package efm.gasolina.gestor_gasolina.service.station;
 
 import efm.gasolina.gestor_gasolina.dto.station.StationAvailabilityDTO;
-import efm.gasolina.gestor_gasolina.model.sesion.RegisterModel;
+import efm.gasolina.gestor_gasolina.model.sesion.Operator;
 import efm.gasolina.gestor_gasolina.model.station.Station;
-import efm.gasolina.gestor_gasolina.repository.sesion.SesionRepository;
+import efm.gasolina.gestor_gasolina.repository.sesion.OperatorRepository;
+
 import efm.gasolina.gestor_gasolina.repository.station.StationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,16 +13,16 @@ import org.springframework.stereotype.Service;
 public class StationAvailability {
 
     @Autowired
-    private SesionRepository sesionRepository;
+    private OperatorRepository operatorRepo;
 
     @Autowired
     private StationRepository stationRepository;
 
     public StationAvailabilityDTO getAvailability(String operatorEmail) {
-        RegisterModel operator = sesionRepository.findByEmail(operatorEmail)
+        Operator operator = operatorRepo.findByEmail(operatorEmail)
             .orElseThrow(() -> new RuntimeException("OPERATOR_NOT_FOUND"));
 
-        Station station = stationRepository.findById(operator.getIdStation())
+        Station station = stationRepository.findById(operator.getId_station())
             .orElseThrow(() -> new RuntimeException("STATION_NOT_FOUND"));
 
         return new StationAvailabilityDTO(
